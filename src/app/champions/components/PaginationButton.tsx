624a9054a375f81f753d71d45aa909e7dcdf92bf
김도현 <hot99009@gmail.com>
2024-10-02 16:07:07 +0900

    버튼 컴포넌트 React.FC 사용X로 리펙토링

--- src/app/champions/components/PaginationButton.tsx
+++ src/app/champions/components/PaginationButton.tsx
@@ -6,14 +6,16 @@ interface PaginationButtonProps {
   label: string;
 }
 
-const PaginationButton: React.FC<PaginationButtonProps> = ({ onClick, disabled, label }) => (
-  <button 
-    className={`p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`} 
-    onClick={onClick} 
-    disabled={disabled}
-  >
-    {label}
-  </button>
-);
+function PaginationButton({ onClick, disabled, label }: PaginationButtonProps) {
+  return (
+    <button 
+      className={`p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`} 
+      onClick={onClick} 
+      disabled={disabled}
+    >
+      {label}
+    </button>
+  );
+}
 
 export default PaginationButton;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import NavLink from '../components/NavLink';
 import '../styles/globals.css';
 import type { Metadata } from 'next';
-import Providers from "@/utils/provider";
+import Providers from "@/lib/provider";
 import { PiBookFill } from "react-icons/pi";
 
 export const metadata: Metadata = {
